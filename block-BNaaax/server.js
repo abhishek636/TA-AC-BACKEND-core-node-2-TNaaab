56e1bab75e2aeb8bd2e3a17a8cf21023b5f518a7
Abhishek Bhatt <bhattabhishek636@gmail.com>
2022-03-15 19:19:06 +0530

    block-BNaaax

--- block-BNaaax/server.js
+++ block-BNaaax/server.js
@@ -1,7 +1,9 @@
 let path = require('path');
 let absolutePath = __dirname;
+var absoluteFile = __filename;
+console.log(absoluteFile);
 let relativePath = './server.js';
 
-var formPath = path.join(__dirname,`${relativePath}`);
+var formPath = path.join(absolutePath,`${relativePath}`);
 
 console.log(formPath, relativePath);
